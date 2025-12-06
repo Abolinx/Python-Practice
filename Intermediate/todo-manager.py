500f43ddafa7bf6412905a411dc8dc6b7c3a6955
Abolinx <abolfazl.karimi.ak83@gmail.com>
2025-12-06 18:52:21 +0330

    feat(todo-manager):Implement delete task function & some changing in other functions

--- Intermediate/todo-manager.py
+++ Intermediate/todo-manager.py
@@ -1,4 +1,4 @@
-user_task = []
+user_tasks = []
 
 class Task:
     def __init__(self, name, description, priority, due_date, state):
@@ -20,44 +20,54 @@ def add_task():
 
     '''
     new_task = Task(input("Please enter name of your task:"), input("Please enter description:"), int(input("Please enter priority(1-7):")), input("Please enter due date (Ex: 1hour):"), input("Please enter state (default:pending):"))
-    user_task.append(new_task)
+    user_tasks.append(new_task)
+
+def view_tasks_S():
+    view_list = []
+    for task in user_tasks:
+        if task.state.lower() == "pending":
+            view_list.append(task)
+    return view_list
+
+def view_tasks_P():
+    view_list = user_tasks
+    for i in range(0, len(view_list)-1):
+        for j in range(len(view_list)-1):
+            if view_list[j].priority > view_list[j+1].priority:
+                view_list[j+1] , view_list[j] = view_list[j] , view_list[j+1] 
+    return view_list
+
+def print_view_tasks(list):
+            
+        print("\n------# view of tasks #-------\n")
+        for index, task in enumerate(list):
+            print(f"{index + 1}.{task.name}:{task.description}-->>{task.state} with {task.priority}th priority")
+            print("--------------------------------------------------")
 
 def view_tasks():
     '''
     Docstring for view_tasks
     '''
-    view_list = []
+
     Filter_or_No = input("Do you want to filter tasks?(yes/no):")
 
     if Filter_or_No.strip().lower() == "yes":
         filter_type = input("state(+) or Priority(-)?:")
     
         if filter_type == "+":
-            for task in user_task:
-                if task.state.lower() == "pending":
-                    view_list.append(task)
-
+           view_list = view_tasks_S()
+        
         elif filter_type == "-":
-            view_list = user_task
-            for i in range(0, len(view_list)-1):
-                for j in range(len(view_list)-1):
-                    if view_list[j].priority > view_list[j+1].priority:
-                        view_list[j+1] , view_list[j] = view_list[j] , view_list[j+1] 
-
+            view_list = view_tasks_P()
+        
         else:
             print("Invalid input!")
             return ""
         
-        print("\n------# view of tasks #-------\n")
-        for index, task in enumerate(view_list):
-            print(f"{index + 1}.{task.name}:{task.description}-->>{task.state} and {task.priority}")
-            print("--------------------------------------------------")
-    
+        print_view_tasks(view_list)
+
     elif Filter_or_No.strip().lower() == "no":
-        print("\n------# view of tasks #-------\n")
-        for index, task in enumerate(user_task):
-            print(f"{index + 1}.{task.name}:{task.description}-->>{task.state} and {task.priority}")
-            print("--------------------------------------------------")
+        print_view_tasks(user_tasks)
 
     else:
         print("Invalid input!")
@@ -72,7 +82,7 @@ def mark_as_complete():
     while True:
         mark = input("do you want marking some tasks?(yes/no):")
         if mark.strip().lower() == "yes":
-            for task in user_task:
+            for task in user_tasks:
                 if task.state.lower() == "pending":
                     view_list.append(task)
             print("\n------# view of tasks #-------\n")
@@ -81,15 +91,18 @@ def mark_as_complete():
                     print("--------------------------------------------------")
             
             task_index = int(input("Which task do you want to mark?"))
-            task_to_complete = view_list.pop(task_index-1)
-            task_to_complete.state = "completed"
-            print(f"The your task ({task_to_complete.name}) is {task_to_complete.state}.")
+            view_list[task_index-1].state = "completed"
+            print(f"The your task ({view_list[task_index-1].name}) is {view_list[task_index-1].state}.")
         else:
             print("Maybe there are still tasks to complete.")
             return ""
 
 def delete_task():
-    pass
+
+    print_view_tasks(user_tasks)
+    task_index_4_del = int(input("witch task you want delete it?"))
+    deleted_task = user_tasks.pop(task_index_4_del-1)
+    print(f"{delete_task.name} is deleted!")
 
 def main():
     while True:    
